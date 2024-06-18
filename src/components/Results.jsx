d491731d5f5f565adafe60fbd00f784c770696e3
Hardik Amareliya <hardikamareliya221@gmail.com>
2024-06-18 19:42:09 -0400

    first commit of investment calculator

---
+++ src/components/Results.jsx
@@ -0,0 +1,32 @@
+
+import { calculateInvestmentResults } from '../util/investment';
+import { formatter } from '../util/investment';
+
+export default function Results({input}) {
+    const resultsData = calculateInvestmentResults(input);
+    const initialInvestment = resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment;
+    return <table id="result">
+        <thead>
+            <tr>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Interested (Year)</th>
+                <th>Total Interest</th>
+                <th>Invested Capital</th>
+            </tr>
+        </thead>
+        <tbody>
+            {resultsData.map(yearData => {
+                const totalInterestValue = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
+                const totalAmountInvested = yearData.valueEndOfYear - totalInterestValue;
+                return <tr key={yearData.year}>
+                    <td>{yearData.year}</td>
+                    <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                    <td>{formatter.format(yearData.interest)}</td>
+                    <td>{formatter.format(totalInterestValue)}</td>
+                    <td>{formatter.format(totalAmountInvested)}</td>
+                </tr>
+            })}
+        </tbody>
+    </table>
+}
